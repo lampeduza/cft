525ea0780531e8758daf69637561c8f3e41ed3e9
lampeduza <lmdogg@mail.ru>
2020-10-22 22:37:57 +0700

    counter

--- rick_and_morty/fs_task.js
+++ rick_and_morty/fs_task.js
@@ -3,13 +3,17 @@ var links = document.querySelectorAll('a');
 var keyUp = function(e) {
   var div = document.querySelector('#autocomplete');
   var value = document.querySelector('#searchInput').value;
+  var counter = document.querySelector('.fs-task__counter');
   div.innerHTML = '';
   if (value.length > 0) {
+    var count = 0;
     links.forEach(function(elem, index) {
       var regex = new RegExp("^" + value, "i");
       if (elem.innerHTML.match(regex)) {
         var click_func = "choice_link('" + links[index].innerHTML + "')";
         div.innerHTML += '<span onclick="' + click_func + '">' + links[index].innerHTML + '</span><br />';
+        count++;
+        counter.textContent = count;
       }
     })
   }
@@ -19,10 +23,3 @@ var choice_link = function(link_item) {
   document.querySelector('#searchInput').value = link_item;
   document.querySelector('#autocomplete').innerHTML = '';
 };
-
-var counter = document.querySelector('fs-task__counter');
-
-
-for (i = 0; i <= links.length - 1; i++) {
-
-}
