46a3fe4dfe659b6b817aa84164eb21563e47e4e2
lampeduza <lmdogg@mail.ru>
2020-10-23 19:10:06 +0700

    Счетчик не обнуляется после выбора ссылки

--- rick_and_morty/fs_task.js
+++ rick_and_morty/fs_task.js
@@ -1,25 +1,35 @@
-var links = document.querySelectorAll('a');
+var links = document.querySelectorAll('a'); // Поиск всех ссылок и распределение их в псевдомассиве
 
 var keyUp = function(e) {
-  var div = document.querySelector('#autocomplete');
-  var value = document.querySelector('#searchInput').value;
-  var counter = document.querySelector('.fs-task__counter');
-  div.innerHTML = '';
-  if (value.length > 0) {
-    var count = 0;
+  var div = document.querySelector('#autocomplete'); // Поиск контейнера
+  var value = document.querySelector('#searchInput').value; // Поиск значения в поле поиска
+  var counter = document.querySelector('.fs-task__counter'); // Поиск счетчика на странице
+  div.textContent = ''; // Записываем в контейнер пустое значение
+
+  if (value.length > 0) { // Если длина значения больше нуля, то
+    var count = 0; // задаем счетчику значение по умолчанию 0, до цикла
     links.forEach(function(elem, index) {
-      var regex = new RegExp("^" + value, "i");
-      if (elem.innerHTML.match(regex)) {
-        var click_func = "choice_link('" + links[index].innerHTML + "')";
-        div.innerHTML += '<span onclick="' + click_func + '">' + links[index].innerHTML + '</span><br />';
-        count++;
-        counter.textContent = count;
+      if (elem) {
+        var regex = new RegExp("^" + value, "i"); // регулярное выражение
+        if (elem.textContent.match(regex)) { // проверяет для каждого элемента, соответствует ли этот элемент введенному в поле поиска значению
+          var click_func = "choice_link('" + links[index].textContent + "')";
+          div.innerHTML += '<span onclick="' + click_func + '">' + links[index].textContent + '</span><br />';
+          count++; // Увлечение счетчика на единицу
+        }
+      }
+      else {
+        count = 0;
       }
     })
+    counter.textContent = count; // Перезаписываем переменную счетчик на новое значение счетчика
+  }
+  else {
+    count = 0;
+    counter.textContent = count; // Перезаписываем переменную счетчик на новое значение счетчика
   }
 }
 
 var choice_link = function(link_item) {
   document.querySelector('#searchInput').value = link_item;
   document.querySelector('#autocomplete').innerHTML = '';
-};
+}
